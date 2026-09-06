1181c36cef983158ab5c6e07f6135f5a32954125
VoytovichKlimentiy <Tyuiu.VoytovichKA@outlook.com>
2026-09-06 19:20:21 +0500

    Task5

---
+++ Tyuiu.VoytovichKA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VoytovichKA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.VoytovichKA.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
